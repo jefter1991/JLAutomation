6391c9e81f3ba03eb01133b05383ab45f0a819b5
Jefter Franciano Lopes <jefterlopes01@gmail.com>
2021-09-05 11:55:04 -0300

    teste

--- backend/src/controllers/RelayController.js
+++ backend/src/controllers/RelayController.js
@@ -12,6 +12,7 @@ module.exports = {
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
           console.log('body:', body); // Print the HTML for the Google homepage.
+          
         });
       
           
