d0bef8b80d71c05f45cd497d494b7b9148ec0f98
Jefter Franciano Lopes <jefterlopes01@gmail.com>
2021-09-11 14:57:54 -0300

    correção no inverter relé

--- backend/src/controllers/RelayController.js
+++ backend/src/controllers/RelayController.js
@@ -11,11 +11,10 @@ module.exports = {
         req(`http://${dev_ip}/gpio/${status}/${port}}`, function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-          console.log('body:', body); // Print the HTML for the Google homepage.
-          
+          console.log('body:', body); // Print the HTML for the Google homepage.  
         });
       
-          
+        return response.json(response.statusCode);   
   } 
 
 }
